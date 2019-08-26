646c5266969ac0bd208301e846811f8d19cdc0c5
Meghannwalsh <meghann.mccool.walsh@gmail.com>
2019-08-26 10:09:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+// function writeCards(names){
+//     let thanks = []
+//     for(let i = 0; i < names.length; i+=1){
+//         name = names[i]
+//         thanks.push("Thank you, " + name + ", for the wonderful surprise gift!")
+//     }
+//     console.log(thanks)
+// }
+function writeCards(namesArray, event) {
+    let thankYouCards = []
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+
+
+function countdown(num){
+    for(let i = num; i >= 0; i-=1){
+        console.log(i);
+    }
+}
+
+writeCards("jen")
+countdown(10); 
